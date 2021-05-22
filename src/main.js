9cd7be5a5a39841a650f2e8ecf821f4d3b341ea5
Matti Nordström <matti.nordstrom@gmail.com>
2021-05-22 17:56:52 +0200

    Can now click on recipes

--- src/main.js
+++ src/main.js
@@ -2,8 +2,8 @@ function render(filter) {
   $( ".cat_menu").removeClass('cat_active');
   $( "#"+filter).addClass('cat_active');
 
-  var newContent = '';
-  for(var i=0; i<recipes.length; i++) {
+  let newContent = '';
+  for(let i=0; i<recipes.length; i++) {
     if(filter && filter === 'Okategoriserat') {
       if(recipes[i].categories){
         continue;
@@ -16,23 +16,40 @@ function render(filter) {
       }
     }
 
-    var cats = recipes[i].categories;
-    var catsOnRowEl = '';
+    let cats = recipes[i].categories;
+    let catsOnRowEl = '';
     if(!cats) {
         cats = '';
     } else {
-      for(var j=0; j<recipes[i].categories.length; j++){
-        catsOnRowEl += '<div class="row_cat '+catMap[recipes[i].categories[j]]+'_cat"></div>';
+      for(let j=0; j<recipes[i].categories.length; j++){
+        catsOnRowEl += '<div class="row_cat ' + catMap[recipes[i].categories[j]] + '_cat"></div>';
       };
     }
 
     newContent += '<div class="row"><div class="row_cats_cont"><div class="row_cats">' + 
-    catsOnRowEl + '</div></div>' + recipes[i].name + 
-    '</div><br/>';
+      catsOnRowEl + '</div></div><div class="recipe_link" onclick="showRecipe(\'' + 
+      recipes[i].name + '\')">' + recipes[i].name + 
+      '</div></div><br/>';
   };
 
   $( ".contentcontainer" ).html(newContent);
 
+}
+
+function showRecipe(recipeName) {
+  $( ".cat_menu").removeClass('cat_active');
+
+  const recipeObj = recipes.find(recipe => recipe.name === recipeName);
+
+  let recipeSource = recipeObj.source;
+  if(recipeObj.source.substring(0,4) === 'http') {
+    recipeSource = '<a target="_blank" href="' + recipeObj.source+'">' + recipeObj.source+'</a>';
+  }
+
+  $( ".contentcontainer" ).html('<h2>' + recipeObj.name +
+    '</h2><i>Källa: ' + recipeSource + 
+    '</i><br/><h3>Ingredienser</h3>' + recipeObj.ingredients + 
+    '<br/><h3>Gör så här</h3>' + recipeObj.directions);
 }
 
 function getRecipes() {
@@ -44,10 +61,10 @@ function getRecipes() {
     render('Alla');
   });
 }
-var recipes = [];
+let recipes = [];
 getRecipes();
 
-var catMap = {
+const catMap = {
   'Kött': 'meat',
   'Fisk': 'fish',
   'Kyckling': 'chicken',
